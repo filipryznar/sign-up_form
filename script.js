98d42c97cef90c75d33d41828c85220ca00ff5e4
filipryznar <103883095+filipryznar@users.noreply.github.com>
2023-11-26 18:30:40 +0100

    add password validation, passwords must match

--- script.js
+++ script.js
@@ -5,3 +5,34 @@ labels.forEach((label) => {
   const labelText = label.textContent;
   label.textContent = labelText.charAt(0).toUpperCase() + labelText.slice(1);
 });
+
+// Get the password input element by its ID
+const passwordInput = document.getElementById("pass");
+const confirmPassInput = document.getElementById("pass2");
+const passwordError = document.getElementsByClassName("error-msg")[0];
+
+// Get the value of the password input
+
+// Function to validate passwords
+function validatePasswords() {
+  var password = pass.value;
+  var confirmPass = pass2.value;
+
+  // Compare passwords
+  if (password === confirmPass) {
+    console.log("Passwords match");
+    // Perform actions when passwords match
+    passwordInput.classList.remove("error");
+    confirmPassInput.classList.remove("error");
+    passwordError.style.display = "none";
+  } else {
+    console.log("Passwords do not match");
+    // Perform actions when passwords do not match
+    passwordInput.classList.add("error");
+    confirmPassInput.classList.add("error");
+    passwordError.style.display = "block";
+  }
+}
+
+passwordInput.addEventListener("input", validatePasswords);
+confirmPassInput.addEventListener("input", validatePasswords);
